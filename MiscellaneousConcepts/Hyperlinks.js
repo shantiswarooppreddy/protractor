1bb3040d124a893ca273abe5f75239a5a7984d3f
shantiswarooppreddy <shanti.s.reddy@gmail.com>
2021-10-13 00:37:30 +0530

    commit

--- MiscellaneousConcepts/Hyperlinks.js
+++ MiscellaneousConcepts/Hyperlinks.js
@@ -1,17 +1,14 @@
-
-
 describe('Sample', function(){
     it("Sample 1", function(){
+        //Launching the practice page
+        browser.get("https://rahulshettyacademy.com/angularpractice/");
 
-        browser.get("www.google.com");
-
-        element.all(by.xpath("href")).each(function(item){
-
-                   item.getText().then(function(text){
-                       console.log(text);
+        element.all(by.css("a[href]")).each(function(item){
+                       browser.wait(protractor.ExpectedConditions.visibilityOf(item), 10000);
+                       item.click();
+                       expect(browser.getCurrentUrl()).toContain("rahulshettyacademy.com");
+                       browser.navigate().back();
                    });
-
         });
 
     });
-});
